b7116b9341319541f3e5b68908077f11e4f7fa94
Khanh Le <le.duy.khanh@framgia.com>
2013-07-02 13:05:12 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GithubDemo::Application.config.secret_token = '4f500b3b0f778e2ff90424f35a45c7294ba026dd2e89252291370edb1621fd2f54364de91cba0dbb916a8fcee1d6d937614b030821c56859924c42ab880a554d'
